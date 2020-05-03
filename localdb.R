790d2a594a40e2d273d64b284b85373e18821d51
Vigneshwar Subramanian <vs347@cornell.edu>
2020-05-03 19:18:23 -0400

    replaced tidyverse import with dplyr + readr imports; all external functions are now called as package::function

--- localdb.R
+++ localdb.R
@@ -1,5 +1,6 @@
 library(rvest)
-library(tidyverse)
+library(dplyr)
+library(readr)
 library(assertthat)
 
 localdb <- function(year,                    # last two digits of desired look-up year
@@ -9,40 +10,33 @@ localdb <- function(year,                    # last two digits of desired look-u
                                              # if F, downloaded files will be removed once database is generated. storage folder will be deleted if empty
                     ){
   if(!missing(locality)) {
-    assert_that(nchar(locality) == 7, msg = 'Locality code must be a valid 7-digit HCFS identification number')
+    assertthat::assert_that(nchar(locality) == 7, msg = 'Locality code must be a valid 7-digit HCFS identification number')
     carrier.number <- substr(as.character(locality), 1, 5)
     localityid <- substr(as.character(locality), 6, 7)
     }
-  assert_that(14<year & year<=20, msg = 'year must be a two digit integer between 14 and 20')
+  assertthat::assert_that(14<year & year<=20, msg = 'year must be a two digit integer between 14 and 20')
   storage.path = paste(storage.path, 'storage', sep = '')
   if(!dir.exists(storage.path)) dir.create(storage.path) # create storage folder if it does not exist
-  assert_that(dir.exists(storage.path), msg = 'Storage folder not found or could not be created; check that provided storage path is valid and R has write permissions')
+  assertthat::assert_that(dir.exists(storage.path), msg = 'Storage folder not found or could not be created; check that provided storage path is valid and R has write permissions')
   # define joining function
-  joinall <- function(inputlist){
+  joinrevisions <- function(inputlist){
+    jointwo <- function(db1, db2){
+      db1 %>% dplyr::left_join(db2, by = c('Year', 'Carrier Number', 'Locality', 'HCPCS Code', 'Modifier', 'Status Code', 'PCTC Indicator', 'Multiple Surgery Indicator', 
+                                                       '50% Therapy Reduction Amount (non-institutional)', '50% Therapy Reduction Amount (institutional)', 'OPPS Indicator')) %>%
+      dplyr::mutate('Facility Fee' = dplyr::coalesce(`Facility Fee.y`, `Facility Fee.x`), 'Non-Facility Fee' = dplyr::coalesce(`Non-Facility Fee.y`, `Non-Facility Fee.x`), 'OPPS Facility Fee' = 
+                      dplyr::coalesce(`OPPS Facility Fee.y`, `OPPS Facility Fee.x`), 'OPPS Non-Facility Fee' = dplyr::coalesce(`OPPS Non-Facility Fee.y`, `OPPS Non-Facility Fee.x`)) %>%
+      dplyr::select(-`Facility Fee.x`, -`Facility Fee.y`, -`Non-Facility Fee.x`, -`Non-Facility Fee.y`, -`OPPS Facility Fee.x`, 
+                    -`OPPS Facility Fee.y`, -`OPPS Non-Facility Fee.x`, -`OPPS Non-Facility Fee.y`) %>%
+      dplyr::full_join(dplyr::anti_join(db2, db1, by = 'HCPCS Code'), by = c("Year", "Carrier Number", "Locality", "HCPCS Code", "Modifier", "PCTC Indicator", "Status Code", 
+                                                                                     "Multiple Surgery Indicator", "50% Therapy Reduction Amount (non-institutional)", "50% Therapy Reduction Amount (institutional)", 
+                                                                                     "OPPS Indicator", "Facility Fee", "Non-Facility Fee", "OPPS Facility Fee", "OPPS Non-Facility Fee")) # addend non-overlapping rows
+    }
     recursivejoin <- function(dblist, index){
       if (index >2){
         db1 <- Recall(dblist, index-1)
-        res<- db1 %>% left_join(dblist[[2]], by = c('Year', 'Carrier Number', 'Locality', 'HCPCS Code', 'Modifier', 'Status Code', 'PCTC Indicator', 'Multiple Surgery Indicator', 
-                                                            '50% Therapy Reduction Amount (non-institutional)', '50% Therapy Reduction Amount (institutional)', 'OPPS Indicator')) %>%
-          mutate('Facility Fee' = coalesce(`Facility Fee.y`, `Facility Fee.x`), 'Non-Facility Fee' = coalesce(`Non-Facility Fee.y`, `Non-Facility Fee.x`), 'OPPS Facility Fee' = 
-                   coalesce(`OPPS Facility Fee.y`, `OPPS Facility Fee.x`), 'OPPS Non-Facility Fee' = coalesce(`OPPS Non-Facility Fee.y`, `OPPS Non-Facility Fee.x`)) %>%
-          select(-`Facility Fee.x`, -`Facility Fee.y`, -`Non-Facility Fee.x`, -`Non-Facility Fee.y`, -`OPPS Facility Fee.x`, 
-                 -`OPPS Facility Fee.y`, -`OPPS Non-Facility Fee.x`, -`OPPS Non-Facility Fee.y`) %>%
-          full_join(anti_join(dblist[[2]], db1, by = 'HCPCS Code'), by = c("Year", "Carrier Number", "Locality", "HCPCS Code", "Modifier", "PCTC Indicator", "Status Code", 
-                                                                           "Multiple Surgery Indicator", "50% Therapy Reduction Amount (non-institutional)", "50% Therapy Reduction Amount (institutional)", 
-                                                                           "OPPS Indicator", "Facility Fee", "Non-Facility Fee", "OPPS Facility Fee", "OPPS Non-Facility Fee")) # addend non-overlapping rows
-        return(res)
+        jointwo(db1, dblist[[index]])
       } else{
-        res<- dblist[[1]] %>% left_join(dblist[[2]], by = c('Year', 'Carrier Number', 'Locality', 'HCPCS Code', 'Modifier', 'Status Code', 'PCTC Indicator', 'Multiple Surgery Indicator', 
-                                                '50% Therapy Reduction Amount (non-institutional)', '50% Therapy Reduction Amount (institutional)', 'OPPS Indicator')) %>%
-          mutate('Facility Fee' = coalesce(`Facility Fee.y`, `Facility Fee.x`), 'Non-Facility Fee' = coalesce(`Non-Facility Fee.y`, `Non-Facility Fee.x`), 'OPPS Facility Fee' = 
-                   coalesce(`OPPS Facility Fee.y`, `OPPS Facility Fee.x`), 'OPPS Non-Facility Fee' = coalesce(`OPPS Non-Facility Fee.y`, `OPPS Non-Facility Fee.x`)) %>%
-          select(-`Facility Fee.x`, -`Facility Fee.y`, -`Non-Facility Fee.x`, -`Non-Facility Fee.y`, -`OPPS Facility Fee.x`, 
-                 -`OPPS Facility Fee.y`, -`OPPS Non-Facility Fee.x`, -`OPPS Non-Facility Fee.y`) %>%
-          full_join(anti_join(dblist[[2]], dblist[[1]], by = 'HCPCS Code'), by = c("Year", "Carrier Number", "Locality", "HCPCS Code", "Modifier", "PCTC Indicator", "Status Code", 
-                                                                                   "Multiple Surgery Indicator", "50% Therapy Reduction Amount (non-institutional)", "50% Therapy Reduction Amount (institutional)", 
-                                                                                   "OPPS Indicator", "Facility Fee", "Non-Facility Fee", "OPPS Facility Fee", "OPPS Non-Facility Fee")) # addend non-overlapping rows
-        return(res)
+        jointwo(dblist[[1]], dblist[[2]])
       }
     }
     recursivejoin(inputlist, length(inputlist))
@@ -51,15 +45,16 @@ localdb <- function(year,                    # last two digits of desired look-u
   
   landingurl <- "https://www.cms.gov/Medicare/Medicare-Fee-for-Service-Payment/PhysicianFeeSched/PFS-National-Payment-Amount-File?items_per_page=100&combine="
   baseurl <- "https://www.cms.gov"
-  links<- read_html(landingurl) %>% html_nodes("a") %>% html_attr("href") 
+  links<- xml2::read_html(landingurl) %>% rvest::html_nodes("a") %>% rvest::html_attr("href") 
   links <- links[grepl(paste('pf.{3,}', year,'[abcd]',sep=''), links, ignore.case=T)]
   links <- links[order(links, substr(links, nchar(links), nchar(links)))] # order alphabetically
   if (!keep.downloads){
     deletepaths <- vector(length = length(links), mode = 'character')
   }
-  mpfs_all<-map(1:length(links), function(x){
+  mpfs_all<-lapply(1:length(links), function(x){
     siteurl <- paste(baseurl, links[x], sep='')
-    dblink <- read_html(siteurl) %>% html_nodes("a") %>% html_attr("href") %>% str_subset("\\.zip")
+    dblink <- xml2::read_html(siteurl) %>% rvest::html_nodes("a") %>% rvest::html_attr("href")
+    dblink <- grep("\\.zip", dblink, value = T)
     dblink <- paste(baseurl, dblink, sep = '')
     path.zip <- paste(storage.path, sub(".*/", "", dblink), sep = '/')
     if(!file.exists(path.zip)) download.file(dblink, path.zip)
@@ -71,9 +66,9 @@ localdb <- function(year,                    # last two digits of desired look-u
                                                       "Facility Fee", "Filler", "PCTC Indicator", "Status Code", "Multiple Surgery Indicator", 
                                                       "50% Therapy Reduction Amount (non-institutional)", "50% Therapy Reduction Amount (institutional)", 
                                                       "OPPS Indicator", "OPPS Non-Facility Fee", "OPPS Facility Fee")) %>%
-      select(-8) # remove filler column
+      dplyr::select(-8) # remove filler column
     ))
-    outputdb <- outputdb %>%  slice(-((nrow(outputdb)-3):nrow(outputdb)))
+    outputdb <- outputdb %>%  dplyr::slice(-((nrow(outputdb)-3):nrow(outputdb)))
     unlink (paste(storage.path, zipped.txt.name, sep = '/')) # delete unzipped .txt
     if(!keep.downloads){
       deletepaths[x] <<- path.zip  # save paths for later cleanup
@@ -88,19 +83,19 @@ localdb <- function(year,                    # last two digits of desired look-u
   }
   if(length(mpfs_all) == 1) {
     res<- mpfs_all[[1]] %>%  
-      mutate(Modifier = forcats::as_factor(Modifier)) %>%
-      mutate(Modifier = recode(Modifier, "  " = "none"))
+      dplyr::mutate(Modifier = as.factor(Modifier)) %>%
+      dplyr::mutate(Modifier = dplyr::recode(Modifier, "  " = "none"))
   } else{
-    res <- joinall(mpfs_all) %>%  
-      mutate(Modifier = forcats::as_factor(Modifier)) %>%
-      mutate(Modifier = recode(Modifier, "  " = "none"))
+    res <- joinrevisions(mpfs_all) %>%  
+      dplyr::mutate(Modifier = as.factor(Modifier)) %>%
+      dplyr::mutate(Modifier = dplyr::recode(Modifier, "  " = "none"))
   }
   if(!missing(locality)){
-    assert_that(carrier.number %in% res$`Carrier Number`, msg = 'Provided locality does not match entries in this database')
-    assert_that(localityid %in% res$Locality, msg = 'Provided locality does not match entries in this database')
+    assertthat::assert_that(carrier.number %in% res$`Carrier Number`, msg = 'Provided locality does not match entries in this database')
+    assertthat::assert_that(localityid %in% res$Locality, msg = 'Provided locality does not match entries in this database')
     res <- res %>% 
-      filter(`Carrier Number` == carrier.number) %>%
-      filter(`Locality` == localityid)
+      dplyr::filter(`Carrier Number` == carrier.number) %>%
+      dplyr::filter(`Locality` == localityid)
   }
   return(res)
 }
