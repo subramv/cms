b6fbc32c19068837c29da72d95aecdf531cc4628
Vigneshwar Subramanian <vs347@cornell.edu>
2020-05-03 15:54:12 -0400

    added recursive joining function to automatically incorporate all addendums together

--- localdb.R
+++ localdb.R
@@ -12,10 +12,39 @@ localdb <- function(year,                    # last two digits of desired look-u
   storage.path = paste(storage.path, 'storage', sep = '')
   if(!dir.exists(storage.path)) dir.create(storage.path) # create storage folder if it does not exist
   assert_that(dir.exists(storage_path), msg = 'Storage folder not found or could not be created; check that provided storage path is valid and R has write permissions')
+  # define joining function
+  joinall <- function(inputlist){
+    recursivejoin <- function(dblist, index){
+      if (index >2){
+        db1 <- Recall(dblist, index-1)
+        res<- db1 %>% left_join(dblist[[2]], by = c('Year', 'Carrier Number', 'Locality', 'HCPCS Code', 'Modifier', 'Status Code', 'PCTC Indicator', 'Multiple Surgery Indicator', 
+                                                            '50% Therapy Reduction Amount (non-institutional)', '50% Therapy Reduction Amount (institutional)', 'OPPS Indicator')) %>%
+          mutate('Facility Fee' = coalesce(`Facility Fee.y`, `Facility Fee.x`), 'Non-Facility Fee' = coalesce(`Non-Facility Fee.y`, `Non-Facility Fee.x`), 'OPPS Facility Fee' = 
+                   coalesce(`OPPS Facility Fee.y`, `OPPS Facility Fee.x`), 'OPPS Non-Facility Fee' = coalesce(`OPPS Non-Facility Fee.y`, `OPPS Non-Facility Fee.x`)) %>%
+          select(-`Facility Fee.x`, -`Facility Fee.y`, -`Non-Facility Fee.x`, -`Non-Facility Fee.y`, -`OPPS Facility Fee.x`, 
+                 -`OPPS Facility Fee.y`, -`OPPS Non-Facility Fee.x`, -`OPPS Non-Facility Fee.y`) %>%
+          full_join(anti_join(dblist[[2]], db1, by = 'HCPCS Code')) # addend non-overlapping rows
+        return(res)
+      } else{
+        res<- dblist[[1]] %>% left_join(dblist[[2]], by = c('Year', 'Carrier Number', 'Locality', 'HCPCS Code', 'Modifier', 'Status Code', 'PCTC Indicator', 'Multiple Surgery Indicator', 
+                                                '50% Therapy Reduction Amount (non-institutional)', '50% Therapy Reduction Amount (institutional)', 'OPPS Indicator')) %>%
+          mutate('Facility Fee' = coalesce(`Facility Fee.y`, `Facility Fee.x`), 'Non-Facility Fee' = coalesce(`Non-Facility Fee.y`, `Non-Facility Fee.x`), 'OPPS Facility Fee' = 
+                   coalesce(`OPPS Facility Fee.y`, `OPPS Facility Fee.x`), 'OPPS Non-Facility Fee' = coalesce(`OPPS Non-Facility Fee.y`, `OPPS Non-Facility Fee.x`)) %>%
+          select(-`Facility Fee.x`, -`Facility Fee.y`, -`Non-Facility Fee.x`, -`Non-Facility Fee.y`, -`OPPS Facility Fee.x`, 
+                 -`OPPS Facility Fee.y`, -`OPPS Non-Facility Fee.x`, -`OPPS Non-Facility Fee.y`) %>%
+          full_join(anti_join(dblist[[2]], dblist[[1]], by = 'HCPCS Code')) # addend non-overlapping rows
+        return(res)
+      }
+    }
+    recursivejoin(inputlist, length(inputlist))
+    }
+  # --------------------------------------------
+  
   landingurl <- "https://www.cms.gov/Medicare/Medicare-Fee-for-Service-Payment/PhysicianFeeSched/PFS-National-Payment-Amount-File?items_per_page=100&combine="
   baseurl <- "https://www.cms.gov"
   links<- read_html(landingurl) %>% html_nodes("a") %>% html_attr("href") 
   links <- links[grepl(paste('pf.{3,}', year,'[abcd]',sep=''), links, ignore.case=T)]
+  links <- links[order(links, substr(links, nchar(links), nchar(links)))] # order alphabetically
   mpfs_all<-map(1:length(links), function(x){
     siteurl <- paste(baseurl, links[x], sep='')
     dblink <- read_html(siteurl) %>% html_nodes("a") %>% html_attr("href") %>% str_subset("\\.zip")
@@ -36,30 +65,15 @@ localdb <- function(year,                    # last two digits of desired look-u
     unlink (paste(storage.path, zipped.txt.name, sep = '/')) # delete unzipped .txt
     return(outputdb)
   })
-  
-  
-  mpfs_rev <- readr::read_delim("PFREV20B.txt", delim=',', col_names = c("Year", "Carrier Number", "Locality", "HCPCS Code", "Modifier", "Non-Facility Fee", 
-                                                                         "Facility Fee", "Filler", "PCTC Indicator", "Status Code", "Multiple Surgery Indicator", 
-                                                                         "50% Therapy Reduction Amount (non-institutional)", "50% Therapy Reduction Amount (institutional)", 
-                                                                         "OPPS Indicator", "OPPS Non-Facility Fee", "OPPS Facility Fee")) %>%
-    select(-8) # remove filler column 
-  mpfs_rev <- mpfs_rev %>% slice(-((nrow(mpfs_rev)-3):nrow(mpfs_rev))) # remove copyright rows
-  
-  
-  mpfs_nat <- readr::read_delim("PFALL20.txt", delim=',', col_names = c("Year", "Carrier Number", "Locality", "HCPCS Code", "Modifier", "Non-Facility Fee", 
-                                                                        "Facility Fee", "Filler", "PCTC Indicator", "Status Code", "Multiple Surgery Indicator", 
-                                                                        "50% Therapy Reduction Amount (non-institutional)", "50% Therapy Reduction Amount (institutional)", 
-                                                                        "OPPS Indicator", "OPPS Non-Facility Fee", "OPPS Facility Fee")) %>%
-    select(-8) # remove filler column 
-  mpfs_nat <- mpfs_nat %>% slice(-((nrow(mpfs_nat)-3):nrow(mpfs_nat))) # remove copyright rows
-  
-  res <- mpfs_nat %>% left_join(mpfs_rev, by = c('Year', 'Carrier Number', 'Locality', 'HCPCS Code', 'Modifier', 'Status Code', 'PCTC Indicator', 'Multiple Surgery Indicator', 
-                                                 '50% Therapy Reduction Amount (non-institutional)', '50% Therapy Reduction Amount (institutional)', 'OPPS Indicator')) %>%
-    mutate('Facility Fee' = coalesce(`Facility Fee.y`, `Facility Fee.x`), 'Non-Facility Fee' = coalesce(`Non-Facility Fee.y`, `Non-Facility Fee.x`), 'OPPS Facility Fee' = 
-             coalesce(`OPPS Facility Fee.y`, `OPPS Facility Fee.x`), 'OPPS Non-Facility Fee' = coalesce(`OPPS Non-Facility Fee.y`, `OPPS Non-Facility Fee.x`)) %>%
-    select(-`Facility Fee.x`, -`Facility Fee.y`, -`Non-Facility Fee.x`, -`Non-Facility Fee.y`, -`OPPS Facility Fee.x`, 
-           -`OPPS Facility Fee.y`, -`OPPS Non-Facility Fee.x`, -`OPPS Non-Facility Fee.y`) %>%
-    full_join(anti_join(mpfs_rev, mpfs_nat, by = 'HCPCS Code')) %>% # addend non-overlapping rows
-    mutate(Modifier = forcats::as_factor(Modifier)) %>%
-    mutate(Modifier = recode(res$Modifier, "  " = "none"))
+  if(length(mpfs_all) == 1) {
+    return(mpfs_all[[1]])
+  } else{
+    res <- joinall(mpfs_all) %>%  
+      mutate(Modifier = forcats::as_factor(Modifier)) %>%
+      mutate(Modifier = recode(Modifier, "  " = "none"))
+    return(res)
+  }
 }
+  
+  
+  
