f445a1e46f78eadb7470099a6135af08b5b5cdc6
Raoul Wadhwa <raoulwadhwa@gmail.com>
2020-05-20 00:15:16 -0400

    added locality as package dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Locality dictionary for 2020 Physician Fee Schedule
+#' 
+#' 
+#' Asterisk indicates that payment locality is serviced by two carriers.
+#' 
+#' @format data frame with 113 rows and 5 columns:
+#' \describe{
+#'   \item{Carrier Number}{character; 5-digit carrier ID}
+#'   \item{Locality}{character; 2-digit locality ID}
+#'   \item{State}{character; uppercase, full U.S. state name}
+#'   \item{Fee Schedule Area}{character; description of fee schedule area}
+#'   \item{Counties}{character; counties included in Fee Schedule Area}
+#' }
+#' @source \url{}
+"locality_dict"
